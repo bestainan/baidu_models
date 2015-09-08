6a3293cc50144002520d773120a4b6bab4c66c0b
Robot <a.robot.n@gmail.com>
2015-09-08 16:45:21 +0800

    Delete webkit_click.js

--- webkit_click.js
+++
@@ -1,9 +0,0 @@
-function dispatch(c, b) {
-    try {
-        var a = document.createEvent("Event");
-        a.initEvent(b, true, true);
-        c.dispatchEvent(a)
-    } catch (d) {
-        alert(d)
-    }
-};
